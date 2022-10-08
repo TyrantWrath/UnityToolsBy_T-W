b8d76e91394c21e48b9e616c8eec84234e7e0465
TyrantWrath <105222983+TyrantWrath@users.noreply.github.com>
2022-10-08 19:07:36 +0300

    Chance Calculation

--- MyUnityTools/Assets/Tyrant's Wrath Frame Work/Scripts/Utilities/TW_HelperMethods.cs
+++ MyUnityTools/Assets/Tyrant's Wrath Frame Work/Scripts/Utilities/TW_HelperMethods.cs
@@ -6,16 +6,52 @@ namespace TyrantsWrath.HelperMethods
 {
     public static class TW_HelperMethods
     {
-        public static Vector2 GetMousePosition2D()
-        {
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            return mousePosition;
-        }
 
-        public static Vector3 GetMousePosition3D()
+
+
+
+
+
+        #region Math Calculation Region
+        public static bool TestChance(int chance, int chanceMax = 100)
         {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            return mousePosition;
+            return UnityEngine.Random.Range(0, chanceMax) < chance;
         }
+        #endregion
+
+
+
+
+
+        #region Mouse Position Region
+        public static Vector2 GetMousePosition2D(Camera mainCam)
+        {
+            if (!mainCam)
+            {
+                Vector2 mousePosition = mainCam.ScreenToWorldPoint(Input.mousePosition);
+                return mousePosition;
+            }
+            else
+            {
+                Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                return mousePosition;
+            }
+
+        }
+        public static Vector3 GetMousePosition3D(Camera mainCam)
+        {
+            if (!mainCam)
+            {
+                Vector3 mousePosition = mainCam.ScreenToWorldPoint(Input.mousePosition);
+                return mousePosition;
+            }
+            else
+            {
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                return mousePosition;
+            }
+
+        }
+        #endregion
     }
 }
