e195f56157c30d9a19aae89fd1d05b011c23c771
TyrantWrath <105222983+TyrantWrath@users.noreply.github.com>
2022-10-08 19:10:55 +0300

    GetMonthNames from int

--- MyUnityTools/Assets/Tyrant's Wrath Frame Work/Scripts/Utilities/TW_HelperMethods.cs
+++ MyUnityTools/Assets/Tyrant's Wrath Frame Work/Scripts/Utilities/TW_HelperMethods.cs
@@ -9,17 +9,12 @@ namespace TyrantsWrath.HelperMethods
 
 
 
-
-
-
         #region Math Calculation Region
         public static bool TestChance(int chance, int chanceMax = 100)
         {
             return UnityEngine.Random.Range(0, chanceMax) < chance;
         }
         #endregion
-
-
 
 
 
@@ -38,6 +33,7 @@ namespace TyrantsWrath.HelperMethods
             }
 
         }
+
         public static Vector3 GetMousePosition3D(Camera mainCam)
         {
             if (!mainCam)
@@ -51,6 +47,30 @@ namespace TyrantsWrath.HelperMethods
                 return mousePosition;
             }
 
+        }
+        #endregion
+
+
+
+        #region  int To string Region
+        public static string GetMonthName(int month)
+        {
+            switch (month)
+            {
+                default:
+                case 0: return "January";
+                case 1: return "February";
+                case 2: return "March";
+                case 3: return "April";
+                case 4: return "May";
+                case 5: return "June";
+                case 6: return "July";
+                case 7: return "August";
+                case 8: return "September";
+                case 9: return "October";
+                case 10: return "November";
+                case 11: return "December";
+            }
         }
         #endregion
     }
