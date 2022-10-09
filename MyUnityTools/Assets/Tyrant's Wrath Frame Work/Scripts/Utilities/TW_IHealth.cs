76797f70373b568f38f2381a7d377d152347f700
TyrantWrath <105222983+TyrantWrath@users.noreply.github.com>
2022-10-09 18:31:09 +0300

    UI Interface, Health Interface class

---
+++ MyUnityTools/Assets/Tyrant's Wrath Frame Work/Scripts/Utilities/TW_IHealth.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TyrantsWrath.InterfaceClasses
+{
+    public interface TW_IHealth
+    {
+        void DealDamage(float damageAmount);
+    }
+}
---
+++ MyUnityTools/Assets/Tyrant's Wrath Frame Work/Scripts/Utilities/TW_IUserInterface.cs
@@ -0,0 +1,15 @@
+namespace TyrantsWrath.InterfaceClasses
+{
+    public interface TW_UI_Interface
+    {
+
+        void OpenUI()
+        {
+
+        }
+        void CloseUI()
+        {
+
+        }
+    }
+}
