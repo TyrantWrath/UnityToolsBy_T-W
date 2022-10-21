23713273ec680207a375c23e2c96dd32f9818b6d
TyrantWrath <105222983+TyrantWrath@users.noreply.github.com>
2022-10-21 15:06:04 +0300

    FPS Counter

---
+++ MyUnityTools/Assets/Tyrant's Wrath Frame Work/Scripts/Utilities/UI/FPSCounter.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TyrantsWrath.UserInterface
+{
+    public class ShowFPS : MonoBehaviour
+    {
+        [SerializeField] private Text fpsText;
+
+        [Tooltip("updateTime is time on how fast FPS should be updated")]
+        [SerializeField] private float updateTime = 0.1f;
+        private float timer;
+        private float updateDeltaTime;
+
+        void Update()
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= updateTime)
+            {
+                updateDeltaTime += (Time.deltaTime - updateDeltaTime) * 0.1f;
+                float fps = 1.0f / updateDeltaTime;
+
+                fpsText.text = Mathf.Ceil(fps).ToString();
+                timer = 0;
+            }
+
+        }
+    }
+}
+
